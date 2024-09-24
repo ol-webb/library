41a062637c995a8108d9b7b1768ea4dd5f016dc1
ol-webb <s2610973@ed.ac.uk>
2024-09-24 11:32:00 +0100

    initial

---
+++ book.py
@@ -0,0 +1,7 @@
+class Book:
+
+    def __init__(self,title,author,ISBN,borrowed):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.borrowed= borrowed
